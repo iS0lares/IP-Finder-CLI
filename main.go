4e9e51d1bd00626f1e6b8a7aca1ef83e637627d4
Alã Damasceno <alacerdan@hotmail.com>
2024-10-29 07:41:19 -0300

    Refactor cleaning up the code

--- main.go
+++ main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"ip-finder/app"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de partida")
 
-	application := app.Generate()
-	if erro := application.Run(os.Args); erro != nil {
-		log.Fatal(erro)
+	if err := app.Generate().Run(os.Args); err != nil {
+		log.Fatal(err.Error())
 	}
 }
